e889783e4dbf293cec7a26f00ddee2f89f47a819
Isaque Misael <sergioisaquepb@gmail.com>
2025-08-27 22:21:30 -0300

    refactor: reorganizar tabelas de eventos e usuários no esquema do banco de dados

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,65 +5,41 @@ import {
   text,
   timestamp,
   serial,
-  json,
   date,
   real,
-  numeric,
   boolean
 } from "drizzle-orm/pg-core";
 
 
-export const eventRegistration = pgTable("eventRegistration", {
+export const event = pgTable("event", {
   id: serial('id').primaryKey().notNull(),
-  imageUrl: text('imageUrl').notNull(),
+  imageUrl: text('image_url'),
   name: varchar('name', { length: 60 }).notNull(),
-  eventTime: text('eventTime').notNull(),
-  registrationDate: timestamp('registrationDate'),
-  ministres: json('ministres'),
-  addInfos: text('AddInfos'),
   value: real('value'),
-  quantityOfTickets: integer('quantityOfTickets'),
-  createdAt: date('createdAt'),
-  updatedAt: date('updatedAt')
+  quantityOfTickets: integer('quantity_of_tickets').notNull(),
+  ministres: text('ministres').notNull(),
+  datetime: date('datetime').defaultNow(),
+  desc: text('desc'),
+  created_at: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow(),
 });
 
-export const peopleRegistration = pgTable("peopleRegistration", {
+export const user = pgTable("user", {
   id: serial('id').primaryKey().notNull(),
-  token: text('token'),
-  name: varchar('name', { length: 100 }).notNull(),
-  loc: text('localization').notNull(),
-  cellphone: numeric('cellphone'),
-  ticketsPurchased: integer('quantityOfTickets'), // QUANTOS TICKETS ELA QUER
-  paidStats: boolean('paidStats'), // SE A PESSOA PAGOU O EVENTO
-  interestStats: boolean('interestStats'), // SE A PESSOA ESTÁ INTERESSADA NO EVENTO
-  createdAt: date('createdAt'),
-  updatedAt: date('updatedAt')
+  name: varchar('name', { length: 60 }).notNull(),
+  email: varchar('email', { length: 60 }).notNull().unique(),
+  password: varchar('password', { length: 60 }).notNull(),
+  created_at: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
 });
 
-export const eventsInf = pgTable("eventsInf", {
-  id: serial('id').primaryKey().notNull(), // VAI REFERENCIAR O ID DO EVENTO (EVENT REGIS.)
-  peopleRegistered: integer('peopleRegistered'), // VAI REFERENCIAR QUANTIDADE TOTAL DE IDS INTERESSADOS DAQUELE EVENTO (QUANTIDADE DE ID'S DE peopleRegistration (interestStats))
-  peoplePaid: integer('peoplePaid'), // VAI REFERENCIAR QUANTIDADE TOTAL DE IDS QUE PAGARAM DAQUELE EVENTO peopleRegistration (paidStats))
-  peopleNotPaid: integer('peopleNotPaid'), // VAI REFERENCIAR QUANTIDADE TOTAL DE IDS QUE NÃO PAGARAM DAQUELE EVENTO (interestStats - paidStats )
-});
 
-export const subscribersInf = pgTable("subscribersInf", {
-  id: serial('id').primaryKey().notNull(), // VAI REFERENCIAR CADA UM DOS ID'S DAS PESSOAS REGISTRADAS
-  // essas informações devem vir juntamente do id da pessoa
-  name: varchar('name', { length: 100 }).notNull(),
-  cellphone: numeric('cellphone'),
-  loc: text('localization').notNull(),
-  paidStats: boolean('paidStats'),
-  ticketsQuantity: integer('ticketsQuantity')
-  //
-});
-
-export const autentication = pgTable("autentication", {
+export const subscribers = pgTable("subscribers", {
   id: serial('id').primaryKey().notNull(),
-  userToken: text('userToken'),
-  userId: text('userId'),
-  email: varchar('email', { length: 100 }),
-  password: varchar('password', { length: 50 }),
-  createdAt: date('createdAt'),
-  updatedAt: date('updatedAt')
+  eventId: integer('event_id').references(() => event.id),
+  userId: integer('user_id').references(() => user.id),
+  paid: boolean('paid'),
+  ticketsQuantity: integer('tickets_quantity').notNull(),
+  created_at: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
 });
